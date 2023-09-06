89c1e4e2dff86ee44d2c5e83459ba26cdb0b0b76
Jared Failor <jfailor@cfu.net>
2023-09-05 21:20:14 -0700

    Again

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -8,7 +8,7 @@ import { startDB } from "../db";
 
 const path = require('path');
 
-const typeDefs = readFileSync(path.join(process.cwd(), './types/types.graphql'), { encoding: 'utf-8' });
+const typeDefs = readFileSync('./src/graphql/types/types.graphql', { encoding: 'utf-8' });
 export interface Context {
   db: Kysely<DB>,
 }
